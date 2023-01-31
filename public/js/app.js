a530783f6547125bb44b09e0e6f6e61d7f13f605
Keqi Chen <ckq009@gmail.com>
2023-01-31 16:00:51 +0000

    Add mobile nav and button to toggle its appearance

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,73 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
+
+  const button1 = document.getElementById("test");
+  
+  // in this way we could apply styles to the same elements with the same class name
+  var arr = Array.from(document.getElementsByClassName("post-msg"))
+
+  const post = document.getElementsByClassName("post-msg")
+  const title = document.getElementById("unique")
+
+  button1.addEventListener("click",function(){
+   title.style.fontSize="100px";
+   console.log(post);
+
+   arr.forEach((i)=>{
+    i.style.backgroundColor= "rgb(100, 100, 100)";
+  })
+   
+  })
+
+
+  const menuButton = document.getElementById("menu");
+  const menuAppear = document.getElementsByClassName("mobile-nav")[0]
+
+  //class always return an array, so needs to specifiy the index, or use forEach.
+
+  menuButton.addEventListener("click",function(){
+
+    if(!menuAppear.classList.contains("appear")) {
+       menuAppear.classList.remove("mobile-nav");
+       menuAppear.classList.add("appear");
+      
+      } else {
+        menuAppear.classList.remove("appear");
+        menuAppear.classList.add("mobile-nav");
+      }
+
+    });
+
+    // const body = document.getElementsByTagName("body");
+    // body.addEventListener("click", function(){
+    //     if(menuAppear.classList.contains("appear")){
+    //       menuAppear.classList.remove("appear");
+    //       menuAppear.classList.add("mobile-nav");
+    //     }
+    //   });
+
+  
+    // can't work properly
+    window.addEventListener("click", function(){
+    
+      console.log("window test")
+    //   if(menuAppear.classList.contains("appear")){
+    //     menuAppear.classList.remove("appear");
+    //     menuAppear.classList.add("mobile-nav");
+    //   }
+  });
+
 });
+
+
+
+
+
+
+  //for some reasons, display does not work
+     //if(menuAppear.style.display === "none") {
+     //  menuAppear.style.display="block";
+     //} else {
+     //   menuAppear.style.display="none";
+  
+  //}

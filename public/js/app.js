5dd77588b85719fac972f8bfc1729fb59a6ac6fe
Keqi Chen <ckq009@gmail.com>
2023-02-01 12:35:50 +0000

    Add user profile page; add escape button for mobile nav

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
 
-  const button1 = document.getElementById("test");
+  // const button1 = document.getElementById("test");
   
-  // in this way we could apply styles to the same elements with the same class name
-  var arr = Array.from(document.getElementsByClassName("post-msg"));
-  const title = document.getElementById("unique");
+  // // in this way we could apply styles to the same elements with the same class name
+  // var arr = Array.from(document.getElementsByClassName("post-msg"));
+  // const title = document.getElementById("unique");
 
-  button1.addEventListener("click",function(){
-   title.style.fontSize="100px";
-   arr.forEach((i)=>{
-    i.style.backgroundColor= "rgb(100, 100, 100)";
-  })
-  })
+  // button1.addEventListener("click",function(){
+  //  title.style.fontSize="100px";
+  //  arr.forEach((i)=>{
+  //   i.style.backgroundColor= "rgb(100, 100, 100)";
+  // })
+  // })
 
   const menuButton = document.getElementById("menu");
   //class always return an array, so needs to specifiy the index, or use forEach.
@@ -33,13 +33,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // create a main tag that wraps everything except the nav bar
     const main = document.getElementsByTagName("main")[0];
-    console.log(main);
     main.addEventListener("click", function(){
         if(menuAppear.classList.contains("appear")){
           menuAppear.classList.remove("appear");
           menuAppear.classList.add("mobile-nav");
         }
       });
+
+      const escapeButton = document.getElementById("escape-button");
+      escapeButton.addEventListener("click", function(){
+          if(menuAppear.classList.contains("appear")){
+            menuAppear.classList.remove("appear");
+            menuAppear.classList.add("mobile-nav");
+          }
+        });
 });
 
   //for some reasons, display does not work
